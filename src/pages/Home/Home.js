4e1a2285a2080a90755eb9c46a7a783b87fdf014
giselly <giselly.avp@gmail.com>
2024-01-29 14:51:24 -0300

    :tada: Commit inicial

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,31 @@
+// Home.js
+
+import React from "react";
+import { UserGroupIcon, SunIcon } from "@heroicons/react/24/outline";
+import LinkedIn from "../../components/Svg/LinkedIn.js";
+
+const Home = () => {
+  return (
+    <div className="font-sans flex flex-col min-h-screen">
+      <header className="flex justify-between">
+        <ul className="flex gap-10">
+          <li>Sobre</li>
+          <li>Projetos</li>
+          <li>Contato</li>
+          <li>More</li>
+        </ul>
+        <ul>
+          <UserGroupIcon className="w-6 h-6" />
+          <SunIcon className="w-6 h-6" />
+          <LinkedIn className="" />
+        </ul>
+      </header>
+
+      <footer className="text-center py-4 mt-auto">
+        <p>&copy; 2024 Meu Portfólio. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
